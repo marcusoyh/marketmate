d999fd3d59868947712cb872cefebc1bc5d97574
marcusoyh <ongmarcus204@gmail.com>
2020-06-11 00:22:08 +0800

    AddItem and List done

--- src/screens/List.js
+++ src/screens/List.js
@@ -1,12 +1,44 @@
 /* eslint-disable prettier/prettier */
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React, { Component, useState, useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import ItemComponent from '../components/ItemComponent';
+import { db } from '../config';
 
 const List = () => {
+
+  let itemsRef = db.ref('/items');
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    getItems();
+  });
+
+  const getItems = () => {
+    itemsRef.on('value', snapshot => {
+      let data = snapshot.val();
+      setItems(Object.values(data));
+      //this.setState({ items });
+    });
+  };
+
   return (
-    <View>
-      <Text>List</Text>
+    <View style={styles.container}>
+      {items.length > 0 ? (
+        <ItemComponent items={items} />
+      ) : (
+        <Text>No items</Text>
+      )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ebebeb',
+  }
+});
+
 export default List;
