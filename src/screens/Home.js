ea5fb010445a3d976b2102c2b3de285b4af4cfa2
marcusoyh <ongmarcus204@gmail.com>
2020-06-10 20:28:23 +0800

    First Commit

---
+++ src/screens/Home.js
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Home Screen</Text>
+      </View>
+    );
+  }
+}
---
+++ src/screens/List.js
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class List extends Component {
+  render() {
+    return (
+      <View>
+        <Text>List</Text>
+      </View>
+    );
+  }
+}
