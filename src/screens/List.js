ca4f5374284813494ea2471f65eb08ffa132e3c4
wunjing <chengwunjing@outlook.com>
2020-06-14 16:46:27 +0800

    list added checkbox

--- src/screens/List.js
+++ src/screens/List.js
@@ -1,50 +1,161 @@
 /* eslint-disable prettier/prettier */
-// import React, { Component, useState, useEffect } from 'react';
-// import { View, Text, StyleSheet } from 'react-native';
+// import React, { Component } from 'react'
+// import { Text, View, TouchableOpacity, StyleSheet, CheckBox } from 'react-native'
+//    import { db } from '../config';
+
+// let itemsRef = db.ref('/items');
+
+// class List extends Component {
+//    state = {
+//       names: [
+//          {
+//             id: 0,
+//             name: 'bread',
+//             checked:false,
+//          },
+//          {
+//             id: 1,
+//             name: 'lala',
+//             checked:false,
+//          },
+//          {
+//             id: 2,
+//             name: 'apple',
+//             checked:false,
+//          },
+//          {
+//             id: 3,
+//             name: 'rice',
+//             checked:false,
+//          }
+//       ]
+//    }
+//    alertItemName = (item) => {
+//       alert(item.name)
+//    }
+
+//    componentDidMount() {
+//           itemsRef.on('value', snapshot => {
+//             let data = snapshot.val();
+//             let items = Object.values(data);
+//             this.setState({ items });
+//           });
+//         }
+
+//    onChangeCheck() {
+//     this.setState({ checked: !this.state.checked})
+//   }
+//    render() {
+//       return (
+//          <View>
+//             {
+//                this.state.names.map((item, index) => (
+//                   <TouchableOpacity
+//                      key = {item.id}
+//                      style = {styles.container}
+//                      onPress = {() => this.alertItemName(item)}>
+//                      <Text style = {styles.text}>
+//                      <CheckBox
+//                             style={styles.checkBox}
+//                             value={item.checked}
+//                             onChange={() => this.onChangeCheck()} />
+//                         {item.name}
+//                      </Text>
+                    
+//                   </TouchableOpacity>
+                
+//                ))
+//             }
+//          </View>
+//       )
+//    }
+// }
+// export default List
+
+// const styles = StyleSheet.create ({
+//    container: {
+//       padding: 10,
+//       marginTop: 3,
+//       backgroundColor: '#d9f9b1',
+//       alignItems: 'center',
+//    },
+//    text: {
+//       color: '#4f603c'
+//    }
+// })
+
+
+// import React, { Component } from 'react';
+// import { View, Text, StyleSheet,CheckBox, TouchableOpacity } from 'react-native';
 // import ItemComponent from '../components/ItemComponent';
+
 // import { db } from '../config';
 
-// const List = () => {
+// let itemsRef = db.ref('/items');
 
-//   let itemsRef = db.ref('/items');
-
-//   const [items, setItems] = useState([]);
-
-//   useEffect(() => {
-//     getItems();
-//   });
-
-//   const getItems = () => {
-//     itemsRef.on('value', snapshot => {
-//       let data = snapshot.val();
-//       setItems(Object.values(data));
-//       //this.setState({ items });
-//     });
+// export default class List extends Component {
+//   state = {
+//     items: [],
+//     checked:false
 //   };
 
-//   return (
-//     <View style={styles.container}>
-//       {items.length > 0 ? (
-//         <ItemComponent items={items} />
-//       ) : (
-//         <Text>No items</Text>
-//       )}
-//     </View>
-//   );
-// };
+//   componentDidMount() {
+//     itemsRef.on('value', snapshot => {
+//       let data = snapshot.val();
+//       let items = Object.values(data);
+//       this.setState({ items });
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <View style={styles.container}>
+     
+
+//           {/* <ItemComponent items={this.state.items} />
+//           <CheckBox
+//           style={styles.checkBox}
+//           value={this.state.checked}
+//           onChange={() => this.onChangeCheck()} /> */}
+//         {
+//                this.state.items.map((item, index) => (
+//                   <TouchableOpacity
+//                      key = {index}
+//                      style = {styles.container}
+//                      >
+//                      <Text>
+//                         {item}
+//                      </Text>
+//                      <CheckBox
+//           style={styles.checkBox}
+//           value={this.state.checked}
+//           onChange={() => this.onChangeCheck()} />
+//                   </TouchableOpacity>
+//                ))
+//             }
+//       </View>
+  
+//     );
+//   }
+
+//   onChangeCheck() {
+//     this.setState({ checked: !this.state.checked})
+//   }
+// }
+
+
 
 // const styles = StyleSheet.create({
 //   container: {
 //     flex: 1,
 //     justifyContent: 'center',
-//     backgroundColor: '#ebebeb',
+//     backgroundColor: '#ebebeb'
 //   }
 // });
 
-// export default List;
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import ItemComponent from '../components/ItemComponent';
+import { View, Text, StyleSheet, CheckBox} from 'react-native';
+
 
 import { db } from '../config';
 
@@ -52,7 +163,8 @@ let itemsRef = db.ref('/items');
 
 export default class List extends Component {
   state = {
-    items: []
+    items: [],
+    checked:false,
   };
 
   componentDidMount() {
@@ -63,23 +175,46 @@ export default class List extends Component {
     });
   }
 
-  render() {
-    return (
-      <View style={styles.container}>
-        {this.state.items.length > 0 ? (
-          <ItemComponent items={this.state.items} />
-        ) : (
-          <Text>No items</Text>
-        )}
-      </View>
-    );
-  }
+  onChangeCheck() {
+       this.setState({ checked: !this.state.checked})
+     }
+
+listM = () => {
+const array= this.state.items.map((item,index) =>{
+   console.log(index);
+   console.log(item);
+   return (item
+   );
+});
+return array.map((element,index)=> {
+   console.log(element);
+   console.log(index);
+   return (
+     <View style={{ margin: 10 }}>
+      <Text style = {styles.text}>{element.name}</Text>
+       <CheckBox 
+       
+            checked={this.state.checked}
+            onPress={() => this.onChangeCheck()}
+          />
+     </View>
+  );
+});
+};
+ render() {
+   return <View style = {styles.container}>{this.listM()}</View>;
+ }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ebebeb'
-  }
-});
+const styles = StyleSheet.create ({
+   container: {
+      padding: 10,
+      marginTop: 3,
+      backgroundColor: '#d9f9b1',
+      alignItems: 'center',
+   },
+   text: {
+      color: '#4f603c',
+      fontSize: 20,
+   }
+})
