07fce251f96cd73e95d943a9d0c6828b48341a7f
marcusoyh <ongmarcus204@gmail.com>
2020-06-15 11:08:20 +0800

    Added datepicker

--- src/screens/AddItem.js
+++ src/screens/AddItem.js
@@ -1,5 +1,6 @@
+/* eslint-disable react-native/no-inline-styles */
 /* eslint-disable prettier/prettier */
-import React, {Component, useEffect, useState} from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import {
   View,
   Text,
@@ -9,18 +10,19 @@ import {
   AlertIOS,
   Alert,
 } from 'react-native';
+import DatePicker from 'react-native-datepicker';
 
 import { db } from '../config';
 import firebase from 'firebase';
 
 const AddItem = () => {
 
-  const [name,setName] = useState('');
-  const [price,setPrice] = useState();
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState();
   const [quantity, setQuantity] = useState();
-  const [date,setDate] = useState();
-  const [notes,setNotes] = useState();
-  
+  const [date, setDate] = useState();
+  const [notes, setNotes] = useState();
+
   // const submit = () => {
   //   console.log('*****');
   //   console.log(name);
@@ -33,11 +35,11 @@ const AddItem = () => {
   const submit = () => {
     const user = firebase.auth().currentUser;
     db.ref('/' + user.uid + '/' + '/items').push({
-      name:name,
-      price:price,
-      quantity:quantity,
-      date:date,
-      notes:notes,
+      name: name,
+      price: price,
+      quantity: quantity,
+      date: date,
+      notes: notes,
     });
     Alert.alert('Item saved successfully');
   };
@@ -50,8 +52,32 @@ const AddItem = () => {
       <TextInput style={styles.itemInput} onChange={e => setPrice(e.nativeEvent.text)} />
       <Text style={styles.title}>Quantity</Text>
       <TextInput style={styles.itemInput} onChange={e => setQuantity(e.nativeEvent.text)} />
-      <Text style={styles.title}>Date</Text>
-      <TextInput style={styles.itemInput} textContentType onChange={e => setDate(e.nativeEvent.text)} />
+
+      <DatePicker
+        style={{width: 200}}
+        date={date}
+        mode="date"
+        placeholder="select date"
+        format="YYYY-MM-DD"
+        minDate="2020-06-01"
+        maxDate="2021-06-01"
+        confirmBtnText="Confirm"
+        cancelBtnText="Cancel"
+        customStyles={{
+          dateIcon: {
+            position: 'absolute',
+            left: 0,
+            top: 4,
+            marginLeft: 0,
+          },
+          dateInput: {
+            marginLeft: 36,
+          }
+          // ... You can check the source to find the other keys.
+        }}
+        onDateChange={(d) => setDate(d) }
+      />
+
       <Text style={styles.title}>Notes</Text>
       <TextInput style={styles.itemInput} textContentType onChange={e => setNotes(e.nativeEvent.text)} />
       <TouchableHighlight
