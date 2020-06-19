0a0aa929aa9c4408cc84d9f1bdaece3b9110b932
marcusoyh <ongmarcus204@gmail.com>
2020-06-20 00:11:43 +0800

    Multiple items with details done
    Multiple items with different fields can be created all at once now in AddList.

--- src/screens/AddList.js
+++ src/screens/AddList.js
@@ -10,7 +10,7 @@ import {
     ScrollView,
 } from 'react-native';
 import { Collapse, CollapseHeader, CollapseBody } from "accordion-collapse-react-native";
-
+import DatePicker from 'react-native-datepicker';
 import { db } from '../config';
 import firebase from 'firebase';
 
@@ -21,6 +21,7 @@ const AddList = () => {
     const [name, setName] = useState('');
     const [items, setItems] = useState([{}]);
     const [itemNames, setItemNames] = useState([]);
+    const [date,setDate] = useState();
 
     const submit = () => {
         const user = firebase.auth().currentUser;
@@ -28,6 +29,7 @@ const AddList = () => {
             items: items,
             //items: texts,
             name: name,
+            date: date,
         });
         Alert.alert('List saved successfully');
     };
@@ -55,6 +57,26 @@ const AddList = () => {
         setItems(newItems);
     };
 
+    const handleChangeQuantity = (e, index) => {
+        console.log('INDEX:');
+        console.log(index);
+        console.log(e.nativeEvent.text);
+        const newItems = items;
+        newItems[index]["quantity"] = e.nativeEvent.text;
+        //newItems[index][1] = e.nativeEvent.text;
+        setItems(newItems);
+    };
+
+    const handleChangeNotes = (e, index) => {
+        console.log('INDEX:');
+        console.log(index);
+        console.log(e.nativeEvent.text);
+        const newItems = items;
+        newItems[index]["price"] = e.nativeEvent.text;
+        //newItems[index][1] = e.nativeEvent.text;
+        setItems(newItems);
+    };
+
     const addItem = () => {
         const newItems = items;
         newItems.push({});
@@ -71,21 +93,46 @@ const AddList = () => {
             <View style={styles.main}>
                 <Text style={styles.title}>List Name</Text>
                 <TextInput
-                    style={styles.itemInput}
+                    style={styles.listNameInput}
+                    placeholder="List Name"
                     onChange={(e) => setName(e.nativeEvent.text)}
                 />
-                <Text style={styles.subtitle}>Items</Text>
-
+                <DatePicker
+                    style={{ width: 200 }}
+                    date={date}
+                    mode="date"
+                    placeholder="select date"
+                    format="YYYY-MM-DD"
+                    minDate="2020-06-01"
+                    maxDate="2021-06-01"
+                    confirmBtnText="Confirm"
+                    cancelBtnText="Cancel"
+                    customStyles={{
+                        dateIcon: {
+                            position: 'absolute',
+                            left: 0,
+                            top: 4,
+                            marginLeft: 0,
+                        },
+                        dateInput: {
+                            marginLeft: 36,
+                        },
+                        // ... You can check the source to find the other keys.
+                    }}
+                    onDateChange={(d) => setDate(d)}
+                />
                 {items.map((item, index) => {
                     return (
                         <View>
                             <Collapse>
                                 <CollapseHeader>
-                                    <Text>Item {index+1}</Text>
+                                    <Text>Item {index + 1}</Text>
                                 </CollapseHeader>
                                 <CollapseBody>
-                                    <TextInput style={styles.itemInput} onChange={(e) => handleChangeName(e, index)} />
-                                    <TextInput style={styles.itemInput} onChange={(e) => handleChangePrice(e, index)} />
+                                    <TextInput style={styles.itemNameInput} placeholder="Name" onChange={(e) => handleChangeName(e, index)} />
+                                    <TextInput style={styles.detailInput} placeholder="Price" onChange={(e) => handleChangePrice(e, index)} />
+                                    <TextInput style={styles.detailInput} placeholder="Quantity" onChange={(e) => handleChangeQuantity(e, index)} />
+                                    <TextInput style={styles.detailInput} placeholder="Notes" onChange={(e) => handleChangeNotes(e, index)} />
                                 </CollapseBody>
                             </Collapse>
                             {/* <TextInput style={styles.itemInput} onChange={(e) => handleChangeName(e, index)} />
@@ -99,7 +146,7 @@ const AddList = () => {
                     style={styles.button}
                     underlayColor="white"
                     onPress={addItem}>
-                    <Text style={styles.buttonText}>Add Item</Text>
+                    <Text style={styles.addItemButtonText}>Add Item</Text>
                 </TouchableHighlight>
 
                 {/* <TouchableHighlight
@@ -111,9 +158,9 @@ const AddList = () => {
 
                 <TouchableHighlight
                     style={styles.button}
-                    underlayColor="green"
+                    underlayColor="white"
                     onPress={submit}>
-                    <Text style={styles.buttonText}>Create</Text>
+                    <Text style={styles.createListButtonText}>Create</Text>
                 </TouchableHighlight>
             </View>
         </ScrollView>
@@ -137,7 +184,7 @@ const styles = StyleSheet.create({
         fontSize: 20,
         textAlign: 'left',
     },
-    itemInput: {
+    listNameInput: {
         height: 50,
         padding: 4,
         marginRight: 5,
@@ -147,7 +194,34 @@ const styles = StyleSheet.create({
         borderRadius: 8,
         color: 'white',
     },
-    buttonText: {
+    itemNameInput: {
+        height: 40,
+        padding: 2,
+        marginLeft: 15,
+        marginRight: 5,
+        fontSize: 23,
+        borderWidth: 1,
+        borderColor: 'white',
+        borderRadius: 8,
+        color: 'white',
+    },
+    detailInput: {
+        height: 40,
+        padding: 2,
+        marginLeft: 15,
+        marginRight: 5,
+        fontSize: 23,
+        borderWidth: 1,
+        borderColor: 'white',
+        borderRadius: 8,
+        color: 'white',
+    },
+    createListButtonText: {
+        fontSize: 18,
+        color: '#8fbc8f',
+        alignSelf: 'center',
+    },
+    addItemButtonText: {
         fontSize: 18,
         color: '#111',
         alignSelf: 'center',
