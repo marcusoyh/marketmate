69e3b0e03acf8032a0e19dcd80b7eb5fd2e2fc20
wunjing <chengwunjing@outlook.com>
2020-07-02 23:10:36 +0800

    can edit item -> press edit first then edit then click edit item

--- src/screens/ViewListDetails.js
+++ src/screens/ViewListDetails.js
@@ -10,11 +10,13 @@ export default class List extends Component {
   state = {
     items: [],
     checked: false,
-    additems: [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]],
+    additems: [[], [], [], [], [], [], [], [], [], [], [], [], [], [], []],
     listname: '',
     date: '',
     search: '',
-    text:'',
+    text: '',
+    edititem:false,
+    editindex:0,
 
   };
 
@@ -64,14 +66,14 @@ export default class List extends Component {
     var newArray = this.state.items[list].items.filter(value => Object.keys(value).length !== 0);
 
     db.ref('/' + uid + '/lists' + '/' + childkey[list]).set({
-      items:newArray,
+      items: newArray,
       name: this.state.listname,
       date: this.state.date
 
     });
     console.log("item is removed");
-    
-   
+
+
   }
 
   onChangeCheck() {
@@ -115,21 +117,64 @@ export default class List extends Component {
 
     });
     Alert.alert('Item added');
-     this.setState(this.state.additems);
-     this.setState(this.state.items);
+    this.setState(this.state.additems);
+    this.setState(this.state.items);
 
   }
 
+  editItem = (list) => {
+    const newItems = this.state.additems;
+    newItems.push({});
+    this.state.additems.map((item, index) => {
+      console.log("additems" + item.name);
+    });
 
+
+    const uid = firebase.auth().currentUser.uid;
+    let itemsRef = db.ref('/' + uid + '/lists');
+    const childkey = [];
+    itemsRef.on('value', snapshot => {
+      snapshot.forEach((child) => {
+        childkey.push(child.key);
+      })
+    });
+
+    db.ref('/' + uid + '/lists' + '/' + childkey[list]).set({
+      items: this.state.additems,
+      name: this.state.listname,
+      date: this.state.date
+
+    });
+    Alert.alert('Item Edited Successfully');
+    this.setState(this.state.additems);
+    this.setState(this.state.items);
+    this.setState({ editindex: 0 })
+    this.setState({ edititem: false })
+  }
+
+  onButtonPress = (index) => {
+    console.log("onpress"+this.state.edititem);
+    if(this.state.edititem == false){
+      this.state.edititem=true;
+      this.setState({ editindex: index })
+    
+    }else{
+      this.state.edititem=false;
+      this.setState({ editindex: index })
+    }
+  }
+ 
+  
   listM = () => {
 
     return this.state.items.map((item, num) => {
       if (num == this.props.navigation.state.params.number && item.items) {
+        console.log("start"+this.state.edititem);
         this.state.listname = item.name;
         this.state.date = item.date;
         const itemList = item.items.length;
         //index for item to add in the array 
-         console.log(itemList + " number to add next");
+        console.log(itemList + " number to add next");
         const indexItem = itemList;
         const deletelist = item.items;
         return (
@@ -150,64 +195,205 @@ export default class List extends Component {
                 <TextInput style={styles.detailInput} placeholder="Quantity" onChangeText={(val) => this.inputValueUpdate(val, 'quantity', indexItem)} />
                 <TextInput style={styles.detailInput} placeholder="Notes" onChangeText={(val) => this.inputValueUpdate(val, 'notes', indexItem)} />
                 <View style={styles.submitButton}>
-            <Button
-              title='Submit'
-              onPress={() => this.addItem(num)}
-              color="#e9967a"
-            />
-            </View>
+                  <Button
+                    title='Submit'
+                    onPress={() => this.addItem(num)}
+                    color="#e9967a"
+                  />
+                </View>
               </CollapseBody>
             </Collapse>
-           
+
             <Text style={styles.itemheader}  >{"Items :"}</Text>
             {item.items.map((info, index) => {
 
               // console.log(this.state.additems);
-              console.log("numbering" + index)
+              console.log("numbering" + index + "edititem" + this.state.edititem)
               this.state.additems[index]['name'] = info.name;
               this.state.additems[index]['quantity'] = info.quantity;
               this.state.additems[index]['price'] = info.price;
               this.state.additems[index]['notes'] = info.notes;
 
               // console.log(this.state.additems[index]['name'] + "additem list");
-
+              if(this.state.edititem == true && this.state.editindex==index){
+                console.log("appear1");
+                
               return (
+                
                 <View >
-     
+
                   <Collapse>
                     <CollapseHeader style={styles.collapseHeader}>
-                    <View style={{flexDirection:"row"}} >
-                    <View style={ {marginRight:20}}>
-                      <CheckBox
-                        checked={this.state.checked}
-                        onPress={() => this.onChangeCheck()} />
+                      <View style={{ flexDirection: "row" }} >
+                        <View style={{ marginRight: 20 }}>
+                          <CheckBox
+                            checked={this.state.checked}
+                            onPress={() => this.onChangeCheck()} />
                         </View>
-                    <View style={{marginLeft:20}, {marginRight:20}}>
-                      <Text style={styles.headercollapse}  >{index+1 +": "}{info.name}</Text>
-                    </View>
-                   
-                        <View style={styles.deleteButton}>
-                        
-                      <Button
-                        title='Delete'
+                        <View style={{ marginLeft: 20 }, { marginRight: 20 }}>
+                          <Text style={styles.headercollapse}  >{index + 1 + ": "}{info.name}</Text>
+                        </View>
 
-                        onPress={() => this.openTwoButtonAlert(index, num, deletelist)}
-                        color="#bc8f8f"
-                      />
+                        <View style={styles.deleteButton}>
+
+                          <Button
+                            title='Delete'
+
+                            onPress={() => this.openTwoButtonAlert(index, num, deletelist)}
+                            color="#bc8f8f"
+                          />
+                        </View>
+                        <View style={styles.deleteButton}>
+
+                          <Button
+                            title='Edit'
+
+                            onPress={() => this.onButtonPress(index)}
+                            color="#bc8f8f"
+                          />
+                          </View>
+                        
                       </View>
-                    </View>
-                     
+                  
                     </CollapseHeader>
 
                     <CollapseBody>
-                      <Text style={styles.detailInput}  >{"Quantity: "}{info.quantity}</Text>
-                      <Text style={styles.detailInput}  >{"Price: "}{info.price}</Text>
-                      <Text style={styles.detailInput}  >{"Notes: "}{info.notes}</Text>
+                      <View style={{ flexDirection: "row" }} >
+                        <View>
+                          <Text style={styles.detailHeader}  >{"Name: "}</Text>
+                        </View>
+                        <View>
+                          {/* <Text style={styles.detailInput}  >{info.name}</Text> */}
+                          <TextInput style={styles.detailInput} placeholder={info.name} onChangeText={(val) => this.inputValueUpdate(val, 'name', index)} />
+                        </View>
+                      </View>
+                      <View style={{ flexDirection: "row" }} >
+                        <View>
+                          <Text style={styles.detailHeader}  >{"Quantity: "}</Text>
+                        </View>
+                        <View>
+                          {/* <Text style={styles.detailInput}  >{info.quantity}</Text> */}
+                          <TextInput style={styles.detailInput} placeholder={info.quantity} onChangeText={(val) => this.inputValueUpdate(val, 'quantity', index)} />
+                        </View>
+                      </View>
+                      <View style={{ flexDirection: "row" }} >
+                        <View>
+                          <Text style={styles.detailHeader}  >{"Price: "}</Text>
+                        </View>
+                        <View>
+                          {/* <Text style={styles.detailInput}  >{info.price}</Text> */}
+                          <TextInput style={styles.detailInput} placeholder={info.price} onChangeText={(val) => this.inputValueUpdate(val, 'price', index)} />
+
+                        </View>
+                      </View>
+                      <View style={{ flexDirection: "row" }} >
+                        <View>
+                          <Text style={styles.detailHeader}  >{"Notes: "}</Text>
+                        </View>
+                        <View>
+                          {/* <Text style={styles.detailInput}  >{info.notes}</Text> */}
+                          <TextInput style={styles.detailInput} placeholder={info.notes} onChangeText={(val) => this.inputValueUpdate(val, 'notes', indexItem)} />
+                        </View>
+                      </View>
+
+                      <View style={styles.submitButton}>
+                  <Button
+                    title='Edit Item'
+                    onPress={() => this.editItem(num)}
+                    color="#e9967a"
+                  />
+                </View>
 
                     </CollapseBody>
                   </Collapse>
                 </View>
               );
+            }else {
+              console.log("appear2");
+              return (
+                
+                <View >
+
+                  <Collapse>
+                    <CollapseHeader style={styles.collapseHeader}>
+                      <View style={{ flexDirection: "row" }} >
+                        <View style={{ marginRight: 20 }}>
+                          <CheckBox
+                            checked={this.state.checked}
+                            onPress={() => this.onChangeCheck()} />
+                        </View>
+                        <View style={{ marginLeft: 20 }, { marginRight: 20 }}>
+                          <Text style={styles.headercollapse}  >{index + 1 + ": "}{info.name}</Text>
+                        </View>
+
+                        <View style={styles.deleteButton}>
+
+                          <Button
+                            title='Delete'
+
+                            onPress={() => this.openTwoButtonAlert(index, num, deletelist)}
+                            color="#bc8f8f"
+                          />
+                        </View>
+                        <View style={styles.deleteButton}>
+
+                          <Button
+                            title='Edit'
+
+                            onPress={() => this.onButtonPress(index)}
+                            color="#bc8f8f"
+                          />
+                          </View>
+                        
+                      </View>
+                  
+                    </CollapseHeader>
+
+                    <CollapseBody>
+                      <View style={{ flexDirection: "row" }} >
+                        <View>
+                          <Text style={styles.detailHeader}  >{"Name: "}</Text>
+                        </View>
+                        <View>
+                          <Text style={styles.detailInput}  >{info.name}</Text>
+                          {/* <TextInput style={styles.detailInput} placeholder={info.name} onChangeText={(val) => this.inputValueUpdate(val, 'name', index)} /> */}
+                        </View>
+                      </View>
+                      <View style={{ flexDirection: "row" }} >
+                        <View>
+                          <Text style={styles.detailHeader}  >{"Quantity: "}</Text>
+                        </View>
+                        <View>
+                          <Text style={styles.detailInput}  >{info.quantity}</Text>
+                          {/* <TextInput style={styles.detailInput} placeholder={info.quantity} onChangeText={(val) => this.inputValueUpdate(val, 'quantity', index)} /> */}
+                        </View>
+                      </View>
+                      <View style={{ flexDirection: "row" }} >
+                        <View>
+                          <Text style={styles.detailHeader}  >{"Price: "}</Text>
+                        </View>
+                        <View>
+                          <Text style={styles.detailInput}  >{info.price}</Text>
+                          {/* <TextInput style={styles.detailInput} placeholder={info.price} onChangeText={(val) => this.inputValueUpdate(val, 'price', index)} /> */}
+
+                        </View>
+                      </View>
+                      <View style={{ flexDirection: "row" }} >
+                        <View>
+                          <Text style={styles.detailHeader}  >{"Notes: "}</Text>
+                        </View>
+                        <View>
+                          <Text style={styles.detailInput}  >{info.notes}</Text>
+                          {/* <TextInput style={styles.detailInput} placeholder={info.notes} onChangeText={(val) => this.inputValueUpdate(val, 'notes', indexItem)} /> */}
+                        </View>
+                      </View>
+
+
+                    </CollapseBody>
+                  </Collapse>
+                </View>
+              );
+              }
             })}
           </View>
 
@@ -221,24 +407,19 @@ export default class List extends Component {
 
             <Text style={styles.text}>{"There are no items"}</Text>
 
+            <Text style={styles.additemstyle}>Add Item</Text>
 
-            <Collapse>
-              <CollapseHeader>
-                <Text>Add Item</Text>
-              </CollapseHeader>
-              <CollapseBody>
-                <TextInput style={styles.detailInput} placeholder="Name" onChangeText={(val) => this.inputValueUpdate(val, 'name', indexItem)} />
-                <TextInput style={styles.detailInput} placeholder="Price" onChangeText={(val) => this.inputValueUpdate(val, 'price', indexItem)} />
-                <TextInput style={styles.detailInput} placeholder="Quantity" onChangeText={(val) => this.inputValueUpdate(val, 'quantity', indexItem)} />
-                <TextInput style={styles.detailInput} placeholder="Notes" onChangeText={(val) => this.inputValueUpdate(val, 'notes', indexItem)} />
-              </CollapseBody>
-            </Collapse>
-            <Button
-              title='Add Item'
-
-              onPress={() => this.addItem(num)}
-              color="#E37399"
-            />
+            <TextInput style={styles.detailInput} placeholder="Name" onChangeText={(val) => this.inputValueUpdate(val, 'name', indexItem)} />
+            <TextInput style={styles.detailInput} placeholder="Price" onChangeText={(val) => this.inputValueUpdate(val, 'price', indexItem)} />
+            <TextInput style={styles.detailInput} placeholder="Quantity" onChangeText={(val) => this.inputValueUpdate(val, 'quantity', indexItem)} />
+            <TextInput style={styles.detailInput} placeholder="Notes" onChangeText={(val) => this.inputValueUpdate(val, 'notes', indexItem)} />
+            <View style={styles.submitButton}>
+              <Button
+                title='Submit'
+                onPress={() => this.addItem(num)}
+                color="#e9967a"
+              />
+            </View>
           </View>
         );
       }
@@ -280,19 +461,19 @@ const styles = StyleSheet.create({
     fontSize: 32,
     textAlign: 'center',
     fontWeight: 'bold'
-  }, 
+  },
   textdate: {
     color: '#bc8f8f',
     fontSize: 20,
     textAlign: 'center',
-  
+
   },
   itemheader: {
     marginTop: 10,
     marginLeft: 10,
     color: '#bc8f8f',
     fontSize: 26,
-  
+
   },
   additemstyle: {
     marginTop: 10,
@@ -307,12 +488,25 @@ const styles = StyleSheet.create({
     marginLeft: 5,
     marginRight: 5,
     fontSize: 18,
-     borderWidth: 2,
+    borderWidth: 2,
     borderColor: '#bc8f8f',
     borderRadius: 6,
     color: 'black',
-    width: 250,
-    alignSelf:'center',
+    width: 230,
+    alignSelf: 'center',
+  },
+  detailHeader: {
+    padding: 5,
+    marginTop: 10,
+    marginLeft: 5,
+
+    fontSize: 18,
+    borderWidth: 2,
+    borderColor: '#fff0f5',
+    borderRadius: 6,
+    color: 'black',
+    width: 90,
+    alignSelf: 'center',
   },
   headercollapse: {
     fontSize: 22,
@@ -324,13 +518,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff0f5',
   },
   submitButton: {
-  marginTop:10,
-    width:150,
-    alignSelf:'center',
-  },  
+    marginTop: 10,
+    width: 150,
+    alignSelf: 'center',
+  },
   deleteButton: {
-   
-    width:80,
-    alignSelf:'center',
+    marginRight:20,
+    width: 80,
+    alignSelf: 'center',
   },
 })
